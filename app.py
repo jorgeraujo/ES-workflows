776beae0a7b1cc972871afa717ec328262bdac64
amaralDaniel <amaral94daniel@gmail.com>
2017-05-21 21:36:01 +0100

    created function to upload.

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from json import dumps
 
 UPLOAD_FOLDER = 'images'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+bucket_name = 'es-workflows-es'
 
 app = Flask(__name__)
 app.secret_key = 'some_secret'
@@ -23,6 +24,36 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def upload_file(request):
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        print('file not in request files')
+        print(request.url)
+        return redirect(request.url)
+    file = request.files['file']
+
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        print('No selected file')
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        print('File and allowed filename')
+        filename = secure_filename(file.filename)
+        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # file.seek(0)
+
+        #send to s3
+        s3client = boto3.client('s3', config=Config(signature_version='s3v4'))
+
+        s3client.put_object(Bucket=bucket_name, Key=file.filename,Body=file)
+
+        url = s3client.generate_presigned_url('get_object', {'Bucket': bucket_name, 'Key': file.filename},(3600 * 24) * 60)
+        return url
+    resp = {"Result": "Something went wrong", "Status Code": 500}
+    return dumps(resp)
+
 
 @app.route('/')
 def index():
@@ -33,58 +64,79 @@ def index():
 def login():
 
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            print('file not in request files')
-            print(request.url)
-            return redirect(request.url)
-        file = request.files['file']
 
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            print('No selected file')
-            return redirect(request.url)
+        url = upload_file(request)
+        print(url)
 
-        if file and allowed_file(file.filename):
-            print('File and allowed filename')
-            filename = secure_filename(file.filename)
-            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # file.seek(0)
+        # Connect to queue (SQS)
+        sqs = boto3.resource("sqs")
+        queue = sqs.get_queue_by_name(QueueName='filita')
 
-            #send to s3
-            s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-            b = s3.Bucket('es-workflows-es')
-            objects = b.objects.all()
+        # Send message to queue
+        queue.send_message(
+            QueueUrl="https://sqs.eu-west-1.amazonaws.com/727565144708/filita",
+            MessageBody=file.filename,
+            DelaySeconds=5,
+        )
 
-            b.put_object(Key=file.filename,Body=file)
-            print ('File saved to S3')
+        # Read message from queue
+        client = boto3.client('sqs')
+        response = client.receive_message(
+        QueueUrl='https://sqs.eu-west-1.amazonaws.com/727565144708/filita',
+        MaxNumberOfMessages=1
+        )
+        print(response)
 
-            # Connect to queue (SQS)
-            sqs = boto3.resource("sqs")
-            queue = sqs.get_queue_by_name(QueueName='filita')
-
-
-            # Send message to queue
-            queue.send_message(
-                QueueUrl="https://sqs.eu-west-1.amazonaws.com/727565144708/filita",
-                MessageBody=file.filename,
-                DelaySeconds=5,
-            )
-
-            # Read message from queue
-            client = boto3.client('sqs')
-            response = client.receive_message(
-            QueueUrl='https://sqs.eu-west-1.amazonaws.com/727565144708/filita',
-            MaxNumberOfMessages=1
-            )
-            print(response)
-
-            return redirect(url_for('payment'))
+        return redirect(url_for('payment'))
     return render_template("login.html")
 
 
 @app.route('/payment', methods=['GET','POST'])
 def payment():
-
     return render_template("payment.html")
+
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+
+        data = request.form
+
+        url = upload_file(request)
+        # #send to s3
+        # s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        # b = s3.Bucket('es-workflows-es')
+        # objects = b.objects.all()
+        #
+        # b.put_object(Key=file.filename,Body=file)
+
+        #do register in sdb
+        simpleDB = boto3.client('sdb')
+
+        import uuid
+        uuid = str(uuid.uuid4())
+
+        resolve = simpleDB.put_attributes(
+            DomainName="ESWorkflows",
+            ItemName=uuid,
+            Attributes=[
+                {
+                    'Name' : 'Name',
+                    'Value' : data["name"],
+                    'Replace' : True
+                },
+                {
+                    'Name' : 'Credit',
+                    'Value' : data["credit"],
+                    'Replace' : True
+                },
+                {
+                    'Name' : 'Photo',
+                    'Value' : url,
+                    'Replace' : False
+                },
+            ]
+        )
+        return "POST"
+
+    return render_template("register.html")
